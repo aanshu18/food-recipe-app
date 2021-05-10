bb27c299b428e1ff17b983f6aa9c42f4e84d191b
Anju Kumari <ashutosh3194@gmail.com>
2021-05-10 18:50:10 +0530

    inprogress

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-function getRandomMeal() {
-    fetch('www.themealdb.com/api/json/v1/1/random.php');
+async function getRandomMeal() {
+    const randomMeal = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+    const randomMealResult = await randomMeal.json();
+    const randomMealData = randomMealResult.meals[0];
+    console.log(randomMealData);
+    loadRandomMeal(randomMealData);
+}
 
+function loadRandomMeal(randomMealData) {
+    let randomMealImage = document.querySelector('.meal-image');
+    randomMealImage.setAttribute('src', `${randomMealData.strMealThumb}`);
+    let randomMealName = document.querySelector('.meal-name');
+    randomMealName.innerHTML = randomMealData.strMeal;
+}
+
+async function getMealById(id) {
+    fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id);
 
 }
 
-function getMealById(id) {
-    fetch('www.themealdb.com/api/json/v1/1/lookup.php?i=' + id);
+async function getMealBySearch(term) { fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + term); }
 
-}
-
-function getMealBySearch(term) { fetch('www.themealdb.com/api/json/v1/1/search.php?s=' + term); }
+getRandomMeal();
